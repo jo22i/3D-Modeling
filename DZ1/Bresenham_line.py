9e2304798f7f3653b6ff08798a62af8216185461
jo22i <99878853+jo22i@users.noreply.github.com>
2023-09-16 12:57:50 +0300

    Добавлены коментарии к коду

--- DZ1/Bresenham_line.py
+++ DZ1/Bresenham_line.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 
 image = Image.new('RGB', (25+1, 25+1))
 
+# Заполнение координатной плоскости серыми квадратами для лучшего визуального наблюдения
 for x in range(0, image.width):
     for y in range(0, image.height):
         if((x%2 == 0 and y%2 == 0) or (x%2 == 1 and y%2 == 1)):
@@ -14,14 +15,16 @@ def Brezenkhem(x0, y0, x1, y1):
     error = 0
     diff = 1
 
+    # Смена координат в случае, если начальная координата дальше по оси х, чем конечная
     if(x0 - x1 > 0):
         x0, x1 = x1, x0
         y0, y1 = y1, y0
 
+    # Проверка на убывание
     if(y0 - y1 > 0):
         diff = -1
 
-    # Угол менее 45
+    # Если угол меньше или равно 45, то увеличиваем/уменьшаем координату y
     if(delta_x >= delta_y):
         y_i = y0
         for x in range(x0, x1 + 1):
@@ -30,8 +33,9 @@ def Brezenkhem(x0, y0, x1, y1):
             if error >= delta_x:
                 y_i += diff
                 error -= 2 * delta_x
-    # Угол больше 45
+    # Иначе - по координате x
     elif(delta_x < delta_y):
+        # Обработка особого случая
         if(diff == -1):
             x0, x1 = x1, x0
             y0, y1 = y1, y0
@@ -48,6 +52,8 @@ y0 = int(input("Координата y первой точки: "))
 x1 = int(input("Координата x второй точки: "))
 y1 = int(input("Координата y второй точки: "))
 Brezenkhem(x0, y0, x1, y1)
+
+# Зеркальное отражение картинки для естественного отображения координатной плоскости
 image = ImageOps.flip(image)
 plt.imshow(image)
 plt.show()
