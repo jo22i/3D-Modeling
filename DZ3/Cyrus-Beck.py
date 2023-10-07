e09a101c70f3d113e8bcb4e7b82dba069c45739d
jo22i <99878853+jo22i@users.noreply.github.com>
2023-10-07 21:15:54 +0300

    Обновлён алгоритм Цируса-Бэка. Необходима дополнительная отладка

--- DZ3/Cyrus-Beck.py
+++ DZ3/Cyrus-Beck.py
@@ -1,7 +1,32 @@
 from PIL import Image, ImageOps
 import matplotlib.pyplot as plt
 
-image = Image.new('RGB', (13+1, 13+1))
+
+# Функция обработки кнопок мыши, отключается как только пользователь введёт координаты отрезка
+def on_click(event):
+    global xA, yA, xB, yB, cid
+
+    # Обработка левой кнопки мыши, отвечающая за получение координат многоугольника
+    if event.button == 1:
+        # image.putpixel((round(event.xdata), round(event.ydata)), (255, 0, 0))
+        polygon_axes.append([round(event.xdata), round(event.ydata)])
+
+    # Обработка правой кнопки мыши, отвечающая за получение координат отрезка
+    elif event.button == 3:
+        # image.putpixel((round(event.xdata), round(event.ydata)), (0, 255, 0))
+        # Получение координат первой точки
+        if(xA == yA == None):
+            xA, yA = round(event.xdata), round(event.ydata)
+        # Получение координаты второй точки, окончание обработки событий мыши
+        else:
+            xB, yB = round(event.xdata), round(event.ydata)
+            plt.disconnect(cid)
+
+polygon_axes = []
+xA, yA, xB, yB = None, None, None, None
+image = Image.new('RGB', (100+1, 100+1))
+image = ImageOps.flip(image)
+cid = plt.connect("button_press_event", on_click)
 
 # Заполнение координатной плоскости серыми квадратами для лучшего визуального наблюдения
 for x in range(0, image.width):
@@ -9,19 +34,106 @@ for x in range(0, image.width):
         if(x%2 == y%2):
             image.putpixel((x, y), (54, 54, 54))
 
-def normal_vector(p_x0, p_y0, p_x1, p_y1):
-    pass
+def Bresenham(x0, y0, x1, y1, color = (255, 255, 255)):
+    delta_x = abs(x1 - x0)
+    delta_y = abs(y1 - y0)
+    error = 0
+    diff = 1
 
-def Cirus_beck(line_cords, points_cords):
-    if(points_cords.count() < 6):
-        print("Error! Points less than 3!")
-        return -1
-    
-    t_params = []
+    # Смена координат в случае, если начальная координата дальше по оси х, чем конечная
+    if(x0 - x1 > 0):
+        x0, x1 = x1, x0
+        y0, y1 = y1, y0
 
-    for i in range(0, len(points_cords)-1):
-        pass
+    # Проверка на убывание
+    if(y0 - y1 > 0):
+        diff = -1
 
-image = ImageOps.flip(image)
+    # Если угол меньше или равно 45, то увеличиваем/уменьшаем координату y
+    if(delta_x >= delta_y):
+        y_i = y0
+        for x in range(x0, x1 + 1):
+            image.putpixel((x, y_i), color)
+            error = error + 2 * delta_y
+            if error >= delta_x:
+                y_i += diff
+                error -= 2 * delta_x
+    # Иначе - по координате x
+    elif(delta_x < delta_y):
+        # Обработка особого случая
+        if(diff == -1):
+            x0, x1 = x1, x0
+            y0, y1 = y1, y0
+        x_i = x0
+        for y in range(y0, y1 + 1):
+            image.putpixel((x_i, y), color)
+            error = error + 2 * delta_x
+            if error >= delta_y:
+                x_i += diff
+                error -= 2 * delta_y
+
+def Cyrus_Beck():
+    global xA, yA, xB, yB, polygon_axes
+
+    t0, t1 = 0, 1
+    Dx, Dy = (xB - xA), (yB - yA)
+
+    for i in range(-1, len(polygon_axes)-1):
+        xp1, yp1 = round(polygon_axes[i][0]), round(polygon_axes[i][1])
+        xp2, yp2 = round(polygon_axes[i+1][0]), round(polygon_axes[i+1][1])
+
+        # Координаты внутренней нормали
+        xN, yN = yp1 - yp2, xp2 - xp1
+
+        # Скалярное произведение внутренней нормали Ni(xN, yN) на вектор pi_O
+        # Необходима для определения положения линии относительно ребра окна
+        Qi = (xA - xp1)*xN + (yA - yp1)*yN
+
+        # Скалярное произведение для определения ориентации отрезка относительно i-й стороны окна
+        Pi = Dx*xN + Dy*yN
+
+        # Вырождение: или точка, или отрезок вне окна
+        if(Pi == 0):
+            # Отрезок вне окна, отсечение закончено
+            # Иначе рассматриваем след. отрезок
+            if(Qi < 0): break
+            continue
+        # Вычисляется параметр t, а также смотрим его положение относительно началов отрезков
+        # Если Pi < 0, то эта точка ближе к концу отрезка, поэтому смотрим наименьшее значение
+        # Иначе точка ближе к началу отрезка, и мы ищем наибольшее значение
+        t = - Qi / Pi
+        if not(0 <= t <= 1): continue
+        if(Pi < 0):
+            t1 = min(t1, t)
+        else:
+            t0 = max(t0, t)
+
+    return t0, t1
+
 plt.imshow(image)
 plt.show()
+
+# Прорисовка изначального положения прямой
+Bresenham(xA, yA, xB, yB, (255, 0, 0))
+
+# Прорисовка многоугольника
+for i in range(-1, len(polygon_axes)-1):
+    Bresenham(polygon_axes[i][0], polygon_axes[i][1], polygon_axes[i+1][0], polygon_axes[i+1][1], (0, 0, 255))
+
+# Получаем параметры начала и конца отсечённого отрезка в виде параметров точек
+t_begin, t_end = Cyrus_Beck()
+
+# Если выполняется условие, то отрисовываем линию, получив координаты точек через параметрическое уравнение,
+# с поправкой на то, что часть отрезка или весь отрезок может оказаться внутри
+if (t_begin <= t_end):
+    if(t_begin != 0):
+        xA, yA = round(xB*t_begin + xA*(1-t_begin)), round(yB*t_begin + yA*(1-t_begin))
+    if(t_end != 1):
+        xB, yB = round(xB*t_end + xA*(1-t_end)), round(yB*t_end + yA*(1-t_end))
+
+    Bresenham(xA, yA, xB, yB)
+
+plt.imshow(image)
+plt.show()
+
+print("OK!")
