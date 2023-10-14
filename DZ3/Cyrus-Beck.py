9bc6c4fe6a4bdffa9cab8b3bb981727a6859b3f7
jo22i <99878853+jo22i@users.noreply.github.com>
2023-10-14 21:53:46 +0300

    Minor changes in Cyrus-Beck algorithm

--- DZ3/Cyrus-Beck.py
+++ DZ3/Cyrus-Beck.py
@@ -21,18 +21,6 @@ def on_click(event):
             xB, yB = round(event.xdata), round(event.ydata)
             plt.disconnect(cid)
 
-polygon_axes = []
-xA, yA, xB, yB = None, None, None, None
-image = Image.new('RGB', (100+1, 100+1))
-image = ImageOps.flip(image)
-cid = plt.connect("button_press_event", on_click)
-
-# Заполнение координатной плоскости серыми квадратами для лучшего визуального наблюдения
-for x in range(0, image.width):
-    for y in range(0, image.height):
-        if(x%2 == y%2):
-            image.putpixel((x, y), (54, 54, 54))
-
 def Bresenham(x0, y0, x1, y1, color = (255, 255, 255)):
     delta_x = abs(x1 - x0)
     delta_y = abs(y1 - y0)
@@ -78,8 +66,8 @@ def Cyrus_Beck():
     Dx, Dy = (xB - xA), (yB - yA)
 
     for i in range(-1, len(polygon_axes)-1):
-        xp1, yp1 = round(polygon_axes[i][0]), round(polygon_axes[i][1])
-        xp2, yp2 = round(polygon_axes[i+1][0]), round(polygon_axes[i+1][1])
+        xp1, yp1 = int(polygon_axes[i][0]), int(polygon_axes[i][1])
+        xp2, yp2 = int(polygon_axes[i+1][0]), int(polygon_axes[i+1][1])
 
         # Координаты внутренней нормали
         xN, yN = yp1 - yp2, xp2 - xp1
@@ -112,6 +100,18 @@ def Cyrus_Beck():
         return tA, tB
     
     return None, None
+
+polygon_axes = []
+xA, yA, xB, yB = None, None, None, None
+image = Image.new('RGB', (30, 30))
+image = ImageOps.flip(image)
+cid = plt.connect("button_press_event", on_click)
+
+# Заполнение координатной плоскости серыми квадратами для лучшего визуального наблюдения
+for x in range(0, image.width):
+    for y in range(0, image.height):
+        if(x%2 == y%2):
+            image.putpixel((x, y), (54, 54, 54))
 
 plt.imshow(image)
 plt.show()
