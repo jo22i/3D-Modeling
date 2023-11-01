f261a42ad2ee1be7df7cd9611ce13174d0bd49b9
jo22i <99878853+jo22i@users.noreply.github.com>
2023-11-01 23:34:40 +0300

    Добавлен шаблон для алгоритма нарисовки 3D объекта из .obj файла

---
+++ DZ6/Obj_to_image.py
@@ -0,0 +1,25 @@
+from PIL import Image, ImageOps
+import matplotlib.pyplot as plt
+from math import sin, cos, pi
+
+def ChangeVector(change_matrix: list, vector: list):
+    pass
+
+def ToRadian(angle: float):
+    return (angle * pi) / 180
+
+def get_move_matrix(dx: float = 0, dy: float = 0, dz: float = 0):
+    pass
+
+def get_scale_matrix(kx: float = 0, ky: float = 0, kz: float = 0):
+    pass
+
+def get_rotate_matrix_X(angle: float, is_radian: bool = False):
+    pass
+
+def get_rotate_matrix_Y(angle: float, is_radian: bool = False):
+    pass
+
+def get_rotate_matrix_Z(angle: float, is_radian: bool = False):
+    pass
+
