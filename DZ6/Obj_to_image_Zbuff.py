ccd8ba88161f1982ff82f1027c23e65f7189cca1
jo22i <99878853+jo22i@users.noreply.github.com>
2023-11-16 17:19:00 +0300

    Minor update

--- DZ6/Obj_to_image_Zbuff.py
+++ DZ6/Obj_to_image_Zbuff.py
@@ -4,20 +4,62 @@ from Bresenham import Bresenham
 from FigureManip import *
 
 
-def is_visible(xi: int, yi: int):
-    global dots, figures, image
+class dot:
+    def __init__(self, cordX: int, cordY: int, cordZ: int):
+        self.x = cordX
+        self.y = cordY
+        self.z = cordZ
 
-    xa, ya, za = dots[fig[0]-1][0], dots[fig[0]-1][1], dots[fig[0]-1][2]
-    xb, yb, zb = dots[fig[1]-1][0], dots[fig[1]-1][1], dots[fig[1]-1][2]
-    xc, yc, zc = dots[fig[2]-1][0], dots[fig[2]-1][1], dots[fig[2]-1][2]
 
-    xa, ya, za = int(xa), int(ya), int(za)
-    xb, yb, zb = int(xb), int(yb), int(zb)
-    xc, yc, zc = int(xc), int(yc), int(zc)
+def get_normal(A: dot, B: dot, C: dot):
+    coefA = (B.y-A.y)*(C.z-A.z) - (B.z-A.z)*(C.y-A.y)
+    coefB = (C.x-A.x)*(B.z-A.z) - (B.x-A.x)*(C.z-A.z)
+    coefC = (B.x-A.x)*(C.y-A.y) - (B.y-A.y)*(C.x-A.x)
 
-    # TO DO: Сделать расчёт принадлежности точки плоскости с помощью векторов нормалей
+    return tuple([coefA, coefB, coefC])
 
-    pass
+
+class figure:
+    def __init__(self, dotA: dot, dotB: dot, dotC: dot, colour: tuple):
+        self.dotA = dotA
+        self.dotB = dotB
+        self.dotC = dotC
+        self.colour = colour
+        self.normalV = get_normal(self.dotA, self.dotB, self.dotC)
+
+
+def is_in_figure(xi: int, yi: int, fig: figure):
+    # Обход вершин идёт по часовой стрелке (стандарт Blender)
+    abV = tuple((fig.dotB.x - fig.dotA.x), (fig.dotB.y - fig.dotA.y))
+    bcV = tuple((fig.dotC.x - fig.dotB.x), (fig.dotC.y - fig.dotB.y))
+    caV = tuple((fig.dotA.x - fig.dotC.x), (fig.dotA.y - fig.dotC.y))
+
+    Nab = tuple(abV[1], -abV[0])
+    Nbc = tuple(bcV[1], -bcV[0])
+    Nca = tuple(caV[1], -caV[0])
+
+    atV = tuple(xi - abV[0], yi - abV[1])
+    btV = tuple(xi - bcV[0], yi - bcV[1])
+    ctV = tuple(xi - caV[0], yi - caV[1])
+
+    if ((Nab[0]*atV[0] + Nab[1]+atV[1] >= 0) and (Nbc[0]*btV[0] + Nbc[1]*btV[1] >= 0) and (Nca[0]*ctV[0] + Nca[1]+ctV[1] >= 0)): return True
+
+    return False
+
+# def is_visible(xi: int, yi: int):
+#     global dots, figures, image
+
+#     xa, ya, za = dots[fig[0]-1][0], dots[fig[0]-1][1], dots[fig[0]-1][2]
+#     xb, yb, zb = dots[fig[1]-1][0], dots[fig[1]-1][1], dots[fig[1]-1][2]
+#     xc, yc, zc = dots[fig[2]-1][0], dots[fig[2]-1][1], dots[fig[2]-1][2]
+
+#     xa, ya, za = int(xa), int(ya), int(za)
+#     xb, yb, zb = int(xb), int(yb), int(zb)
+#     xc, yc, zc = int(xc), int(yc), int(zc)
+
+#     # TO DO: Сделать расчёт принадлежности точки плоскости с помощью векторов нормалей
+
+#     pass
 
 
 dots = []
